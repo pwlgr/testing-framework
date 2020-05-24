8d553f841f9ed61e5f58740a8cde57118df5e42e
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-24 18:06:18 +0200

    add colors for console messages for better ux

--- runner.js
+++ runner.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 class Runner {
 	constructor() {
@@ -8,7 +9,27 @@ class Runner {
 
 	async runTests() {
 		for (let file of this.testFiles) {
-			require(file.name);
+			console.log(chalk.gray(`---- ${file.name}`));
+			const beforeEaches = [];
+			global.beforeEach = (fn) => {
+				beforeEaches.push(fn);
+			};
+
+			global.it = (desc, fn) => {
+				beforeEaches.forEach((func) => func());
+				try {
+					fn();
+					console.log(chalk.green(`OK - ${desc}`));
+				} catch (err) {
+					console.log(chalk.red(`X - ${desc}`));
+					console.log(chalk.red('\t', err.message));
+				}
+			};
+			try {
+				require(file.name);
+			} catch (err) {
+				console.log(chalk.red(err));
+			}
 		}
 	}
 
