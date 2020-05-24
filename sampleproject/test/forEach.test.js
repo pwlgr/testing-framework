f80e7f56b6496cd3068f57857f9fbd34f5622fcd
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-24 17:28:51 +0200

    implement it and beforeEach global functions

--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -1,8 +1,13 @@
 const assert = require('assert');
 const { forEach } = require('../index');
 
+let numbers;
+
+beforeEach(() => {
+	numbers = [ 1, 2, 3 ];
+});
+
 it('should sum array', () => {
-	const numbers = [ 1, 2, 3 ];
 	let total = 0;
 
 	forEach(numbers, (value) => {
@@ -10,4 +15,12 @@ it('should sum array', () => {
 	});
 
 	assert.strictEqual(total, 6);
+	numbers.push(3);
+	numbers.push(3);
+	numbers.push(3);
+	numbers.push(3);
+});
+
+it('beforeEach is ran each time', () => {
+	assert.strictEqual(numbers.length, 3);
 });
