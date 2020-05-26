aa3a25257af8517a80b5a5cfb68fe672687203ca
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-26 23:47:22 +0200

    refactor render function to wait after the dom finished loading

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -6,3 +6,16 @@ it('has a text input', async () => {
 
 	assert(input);
 });
+
+it('shows a success with valid email', async () => {
+	const dom = await render('index.html');
+
+	const input = dom.window.document.querySelector('input');
+
+	input.value = 'adaddd@asd.com';
+	dom.window.document.querySelector('form').dispatchEvent(new dom.window.Event('submit'));
+
+	const h1 = dom.window.document.querySelector('h1');
+
+	console.log(h1.innerHTML);
+});
